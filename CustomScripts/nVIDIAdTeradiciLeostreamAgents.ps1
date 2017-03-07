ba3078c0a546d4558a8901681115e8bc5fd06ae5
Dwai Banerjee <dwaiba@microsoft.com>
2017-03-07 16:34:01 +0100

    Update nVIDIAdTeradiciLeostreamAgents.ps1

--- CustomScripts/nVIDIAdTeradiciLeostreamAgents.ps1
+++ CustomScripts/nVIDIAdTeradiciLeostreamAgents.ps1
@@ -11,7 +11,7 @@ $nvidiaazure = $args[7]
 $softwareExeName = $args[8]
 $omsWorkSpaceId = $args[9]
 $omsWorkSpaceKey = $args[10]
-$dockerVer = $args[11]
+$nuget4dockerVer = $args[11]
 $registryPath = "HKLM:\Software\Teradici\PCoIP\pcoip_admin"
 $Name = "pcoip.max_encode_threads"
 $value = "8"
@@ -140,11 +140,12 @@ else
   Write-Host  "No Registry entry required ."
 }
 
-net stop nvsvc
+<#net stop nvsvc
 Write-Host "Stopped NVIDIA Display Driver"
 Start-Sleep -s 240
 net start nvsvc
-Write-Host "Starting NVIDIA Display Driver"
+Write-Host "Starting NVIDIA Display Driver"#>
+
 <#OMS Hook#>
 if ($omsWorkSpaceId -and $omsWorkSpaceKey) {
   $omsAgentUrl = [System.String]::Format("https://{0}.blob.core.windows.net/{1}/MMASetup-AMD64.exe", $storageAcc, $conName)
@@ -156,29 +157,10 @@ if ($omsWorkSpaceId -and $omsWorkSpaceKey) {
   .\MMASetup-AMD64.exe /Q:A /R:N /C:"setup.exe /qn ADD_OPINSIGHTS_WORKSPACE=1 OPINSIGHTS_WORKSPACE_ID=$omsWorkSpaceId  OPINSIGHTS_WORKSPACE_KEY=$omsWorkSpaceKey AcceptEndUserLicenseAgreement=1"
 }
 
-<# Install Docker latest Dev for Windows> 
-$Date = Get-Date
-Write-Host "You input is  '$dockerVer' on '$Date'"
-<#Write-Host "Un-Installing existing dockerd and Installing Docker for Windows version '$dockerVer' on '$Date'"
-Stop-Service Docker
-dockerd --unregister-service
-Remove-Item "C:\Program Files\docker" -Force -Recurse
-rm C:\ProgramData\docker\docker.pid#>
-<#Enable-WindowsOptionalFeature -Online -FeatureName containers -All -NoRestart
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart
-Invoke-WebRequest "https://master.dockerproject.org/windows/amd64/docker-$dockerVer.zip" -OutFile "$env:TEMP\docker-$dockerVer.zip" -UseBasicParsing
-Expand-Archive -Path "$env:TEMP\docker-$dockerVer.zip" -DestinationPath $env:ProgramFiles
-# For quick use, does not require shell to be restarted.
-$env:path += ";c:\program files\docker"
-# For persistent use, will apply even after a reboot.
-[Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Program Files\Docker", [EnvironmentVariableTarget]::Machine)
-dockerd --register-service
-Start-Service Docker
-Write-Host "Installed Docker for Windows version '$dockerVer' on '$Date'. Welcome !!" #>
-
 <#Install Docker EE for Windows Server 2016#>
+Install-PackageProvider -Name NuGet -MinimumVersion $nuget4dockerVer -Force
 Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
-set-executionpolicy remotesigned -Force
+<#set-executionpolicy remotesigned -Force#>
 Install-Package -Name docker -ProviderName DockerMsftProvider -Force
 
 <# Reboot in 60 seconds #>
