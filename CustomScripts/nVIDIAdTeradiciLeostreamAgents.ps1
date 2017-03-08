241c39b770d7439ca74a0db6b976cfc111dfa6ef
Dwai Banerjee <dwaiba@microsoft.com>
2017-03-08 12:00:21 +0100

    Update nVIDIAdTeradiciLeostreamAgents.ps1

--- CustomScripts/nVIDIAdTeradiciLeostreamAgents.ps1
+++ CustomScripts/nVIDIAdTeradiciLeostreamAgents.ps1
@@ -25,21 +25,6 @@ $arguments = @(
 New-Item -Path $dest -ItemType directory
 New-Item -Path $opendtectloc -ItemType directory
 
-$teradiciAgentUrl = [System.String]::Format("https://{0}.blob.core.windows.net/{1}/PCoIP_agent_release_installer_{2}_graphics.exe", $storageAcc, $conName, $teradiciAgentVer)
-$teradiciExeName = [System.IO.Path]::GetFileName($teradiciAgentUrl)
-$teradiciExePath = [System.String]::Format("{0}{1}", $dest, $teradiciExeName)
-$leostreamAgentUrl = [System.String]::Format("https://{0}.blob.core.windows.net/{1}/LeostreamAgentSetup{2}.exe", $storageAcc, $conName, $leostreamAgentVer)
-$leostreamExeName = [System.IO.Path]::GetFileName($leostreamAgentUrl)
-$leostreamExePath = [System.String]::Format("{0}{1}", $dest, $leostreamExeName)
-Write-Host "The Teradici Agent exe  Url  is '$teradiciAgentUrl'"
-Write-Host "The Teradici Agent exe name is '$teradiciExeName'"
-Write-Host "The Teradici Agent exe downloaded location is '$teradiciExePath'"
-Write-Host "The Leostream Agent exe Url is '$leostreamAgentUrl'"
-Write-Host "The Leostream Agent exe name is '$leostreamExeName'"
-Write-Host "The Leostream Agent exe downloaded location is '$leostreamExePath'"
-wget $teradiciAgentUrl -OutFile $teradiciExePath
-wget $leostreamAgentUrl -OutFile $leostreamExePath
-Start-Sleep -s 360
 
 function Unzip
 {
@@ -115,6 +100,39 @@ Set-Location $NVIDIAfolder
 Set-ExecutionPolicy Unrestricted -force
 .\setup.exe -s -noreboot -clean
 Start-Sleep -s 180
+
+if ($license) {
+$teradiciAgentUrl = [System.String]::Format("https://{0}.blob.core.windows.net/{1}/PCoIP_agent_release_installer_{2}_graphics.exe", $storageAcc, $conName, $teradiciAgentVer)
+$teradiciExeName = [System.IO.Path]::GetFileName($teradiciAgentUrl)
+$teradiciExePath = [System.String]::Format("{0}{1}", $dest, $teradiciExeName)
+$leostreamAgentUrl = [System.String]::Format("https://{0}.blob.core.windows.net/{1}/LeostreamAgentSetup{2}.exe", $storageAcc, $conName, $leostreamAgentVer)
+$leostreamExeName = [System.IO.Path]::GetFileName($leostreamAgentUrl)
+$leostreamExePath = [System.String]::Format("{0}{1}", $dest, $leostreamExeName)
+Write-Host "The Teradici Agent exe  Url  is '$teradiciAgentUrl'"
+Write-Host "The Teradici Agent exe name is '$teradiciExeName'"
+Write-Host "The Teradici Agent exe downloaded location is '$teradiciExePath'"
+Write-Host "The Leostream Agent exe Url is '$leostreamAgentUrl'"
+Write-Host "The Leostream Agent exe name is '$leostreamExeName'"
+Write-Host "The Leostream Agent exe downloaded location is '$leostreamExePath'"
+wget $teradiciAgentUrl -OutFile $teradiciExePath
+wget $leostreamAgentUrl -OutFile $leostreamExePath
+Start-Sleep -s 360
+$teradiciAgentUrl = [System.String]::Format("https://{0}.blob.core.windows.net/{1}/PCoIP_agent_release_installer_{2}_graphics.exe", $storageAcc, $conName, $teradiciAgentVer)
+$teradiciExeName = [System.IO.Path]::GetFileName($teradiciAgentUrl)
+$teradiciExePath = [System.String]::Format("{0}{1}", $dest, $teradiciExeName)
+$leostreamAgentUrl = [System.String]::Format("https://{0}.blob.core.windows.net/{1}/LeostreamAgentSetup{2}.exe", $storageAcc, $conName, $leostreamAgentVer)
+$leostreamExeName = [System.IO.Path]::GetFileName($leostreamAgentUrl)
+$leostreamExePath = [System.String]::Format("{0}{1}", $dest, $leostreamExeName)
+Write-Host "The Teradici Agent exe  Url  is '$teradiciAgentUrl'"
+Write-Host "The Teradici Agent exe name is '$teradiciExeName'"
+Write-Host "The Teradici Agent exe downloaded location is '$teradiciExePath'"
+Write-Host "The Leostream Agent exe Url is '$leostreamAgentUrl'"
+Write-Host "The Leostream Agent exe name is '$leostreamExeName'"
+Write-Host "The Leostream Agent exe downloaded location is '$leostreamExePath'"
+wget $teradiciAgentUrl -OutFile $teradiciExePath
+wget $leostreamAgentUrl -OutFile $leostreamExePath
+Start-Sleep -s 360
+
 & $teradiciExePath /S /NoPostReboot
 Start-Sleep -s 90 
 Write-Host "teradiciagent install over"
@@ -139,6 +157,10 @@ else
 { 
   Write-Host  "No Registry entry required ."
 }
+
+}
+
+
 
 <#net stop nvsvc
 Write-Host "Stopped NVIDIA Display Driver"
